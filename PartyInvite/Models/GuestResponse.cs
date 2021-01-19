7095e176f2c0845f1f8113a378e89b0de4d7a808
wisestMe <wyhzest@gmail.com>
2021-01-19 09:08:29 +0100

    add link to form

--- PartyInvite/Models/GuestResponse.cs
+++ PartyInvite/Models/GuestResponse.cs
@@ -10,6 +10,6 @@ namespace PartyInvite.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public bool WillAttend { get; set; }
+        public bool? WillAttend { get; set; }
     }
 }
