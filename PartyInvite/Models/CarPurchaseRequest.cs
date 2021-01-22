53098b4b50108781bacf73b0b383351ae92554ad
wisestMe <wyhzest@gmail.com>
2021-01-22 14:28:31 +0100

    add car purchase domain model

---
+++ PartyInvite/Models/CarPurchaseRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvite.Models
+{
+    public class CarPurchaseRequest
+    {
+        public string CarManufacturer { get; set; }
+        public string CarType { get; set; }
+        public string CarModel { get; set; }
+        public DateTime YearManufactured { get; set; }
+    }
+}
