d4b09c468cc697155c565ed75b2255c289121dd7
wisestMe <wyhzest@gmail.com>
2021-01-23 07:25:57 +0100

    create favourite artist response model

---
+++ PartyInvite/Models/FavouriteArtistResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvite.Models
+{
+    public class FavouriteArtistResponse
+    {
+        public string UserName { get; set; }
+        public string ArtistName { get; set; }
+        public string YourFavouriteTrack { get; set; }
+        public int NumberOfAlbum { get; set; }
+        public DateTime ArtistDOB { get; set; }
+    }
+}
