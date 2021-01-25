e77946f6a6150fd9385b5a46b3110a3f5e9f409d
wisestMe <wyhzest@gmail.com>
2021-01-25 11:40:49 +0100

    add validations

--- PartyInvite/Models/GuestResponse.cs
+++ PartyInvite/Models/GuestResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,13 @@ namespace PartyInvite.Models
 {
     public class GuestResponse
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public bool? WillAttend { get; set; }
     }
 }
