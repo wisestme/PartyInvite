59a8821abf7e994236abad5574ae5293d542d818
wisestMe <wyhzest@gmail.com>
2021-01-26 15:31:02 +0100

    create new project and add reference

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Another MVC project. Keep moving Wisest";
+        }
+    }
+}
