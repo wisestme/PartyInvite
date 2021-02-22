66f8573b5aa1f3f18dfcf330b4086774c938b470
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-22 11:15:00 +0100

    add dummy controller

---
+++ PragimASP/Controllers/NotHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PragimASP.Controllers
+{
+    public class NotHomeController : Controller
+    {
+        // GET: NotHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
