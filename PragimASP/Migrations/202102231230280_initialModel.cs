dcea08d596d0dad24c2877b1db7f193e73c0dd79
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-23 13:39:36 +0100

    map model to table

---
+++ PragimASP/Migrations/202102231230280_initialModel.cs
@@ -0,0 +1,28 @@
+﻿namespace PragimASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        EmployeeId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Gender = c.String(),
+                        City = c.String(),
+                    })
+                .PrimaryKey(t => t.EmployeeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
--- PragimASP/Models/Employee.cs
+++ PragimASP/Models/Employee.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace PragimASP.Models
 {
+    [Table("Employees")]
     public class Employee
     {
         public int EmployeeId { get; set; }
