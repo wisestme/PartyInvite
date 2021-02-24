fe3b55128a9cc78642d6d4415b9a60b176051b20
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-24 10:36:49 +0100

    ubdate database

---
+++ PragimASP/Migrations/202102240935327_addCarModel.cs
@@ -0,0 +1,16 @@
+namespace PragimASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addCarModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ PragimASP/Models/CarContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PragimASP.Models
+{
+    public class CarContext : DbContext
+    {
+        public DbSet<Car> cars { get; set; }
+    }
+}
