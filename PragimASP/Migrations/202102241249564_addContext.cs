2ed7f04fccdb2dcd4dba2739896987f9f294833d
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-24 14:38:57 +0100

    check db error

---
+++ PragimASP/Migrations/202102241249564_addContext.cs
@@ -0,0 +1,16 @@
+namespace PragimASP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addContext : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
