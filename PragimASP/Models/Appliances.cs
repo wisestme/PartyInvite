02627cfc7a0b4daae8683acd1351292d559145c9
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-24 11:47:38 +0100

    add appliance class

---
+++ PragimASP/Models/Appliances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PragimASP.Models
+{
+    public class Appliances
+    {
+        public int ApplianceID { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string DateInstalled { get; set; }
+    }
+}
