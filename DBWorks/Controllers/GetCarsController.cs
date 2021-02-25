08ca8d05af65052bb7b798eaa800ad0db37350ce
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-25 10:21:20 +0100

    get list of cars

---
+++ DBWorks/Controllers/GetCarsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DBWorks.Models;
+
+namespace DBWorks.Controllers
+{
+    public class GetCarsController : Controller
+    {
+        // GET: GetCars
+        public ActionResult Index()
+        {
+            AccessDB carAccessDb = new AccessDB();
+            List<Car> cars = carAccessDb.Cars.ToList();
+            return View(cars);
+        }
+    }
+}
