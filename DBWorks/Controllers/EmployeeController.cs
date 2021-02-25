73e4bcdd5ad557f82717e01ed31819311056da1b
wisestme01 <c.nwagwu@genesystechhub.com>
2021-02-25 09:02:38 +0100

    get list of employees

--- DBWorks/Controllers/EmployeeController.cs
+++ DBWorks/Controllers/EmployeeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DBWorks.Models;
 
 namespace DBWorks.Controllers
 {
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult GetEmployees()
         {
-            return View();
+            AccessDB employeeAccessDb = new AccessDB();
+            List<Employee> employees = employeeAccessDb.Employees.ToList();
+            return View(employees);
         }
     }
 }
