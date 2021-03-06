bb2c560b7dec1125829767a536a8ce2d6c7cc26a
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-06 22:03:52 +0100

    service to add car.

---
+++ DBWorks/Repository/CarService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DBWorks.Entities;
+using DBWorks.IdentityEntities;
+using DBWorks.Services;
+
+namespace DBWorks.Repository
+{
+    public class CarService : ICarService
+    {
+        public void AddCar(Car car)
+        {
+            AccessDB accessDB = new AccessDB();
+            accessDB.Cars.Add(car);
+            accessDB.SaveChanges();
+        }
+    }
+}
---
+++ DBWorks/Services/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DBWorks.Entities;
+
+namespace DBWorks.Services
+{
+    public interface ICarService
+    {
+        void AddCar(Car car);
+    }
+}
