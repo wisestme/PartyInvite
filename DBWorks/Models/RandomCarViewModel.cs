f5f24d5823549005c7dd0bdc55ad4af6a820db66
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-08 14:18:18 +0100

    add view model

---
+++ DBWorks/Models/RandomCarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DBWorks.Entities;
+
+namespace DBWorks.Models
+{
+    public class RandomCarViewModel
+    {
+        public Employee Employee { get; set; }
+        public Car Car { get; set; }
+    }
+}
