558a092156c52c20a062355773e622b7151a3876
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-10 09:03:26 +0100

    add department

---
+++ DBWorks/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBWorks.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
