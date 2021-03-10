99300410dfec2d2ff6a1cadd9f4d0a67547cb4e8
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-10 09:17:36 +0100

    create department table

--- DBWorks/IdentityEntities/AccessDB.cs
+++ DBWorks/IdentityEntities/AccessDB.cs
@@ -7,5 +7,6 @@ namespace DBWorks.IdentityEntities
     {
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Car> Cars { get; set; }
+        public DbSet<Department> Departments { get; set; }
     }
 }
