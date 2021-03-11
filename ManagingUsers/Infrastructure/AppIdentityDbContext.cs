bf21c07099e60cf50eca3b7c76266806fa8a6d7d
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-11 14:03:21 +0100

    configure db context class

---
+++ ManagingUsers/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ManagingUsers.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ManagingUsers.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("DbIdentity")
+        {
+
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+    }
+
+    public class DbIdentityInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(AppIdentityDbContext context)
+        {
+
+        }
+    }
+}
