1a206cc70c81ceac871671e465af0bea8efd87ab
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-11 09:26:41 +0100

    implement authorization

---
+++ FashionPlace/Controllers/TestIdentityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionPlace.Controllers
+{
+    [Authorize]
+    public class TestIdentityController : Controller
+    {
+        // GET: TestIdentity
+        
+        public ActionResult Identity()
+        {
+            return Content("We are the identity folks!!!");
+        }
+
+        public ActionResult NonIdentity()
+        {
+            return Content("We don't bother about identity");
+        }
+    }
+}
