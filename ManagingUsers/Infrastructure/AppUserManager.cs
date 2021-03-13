d8a108b9370a026680fcef4eaeee3fbe74ccfa5a
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-13 22:13:20 +0100

    create identity config

---
+++ ManagingUsers/Infrastructure/AppUserManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ManagingUsers.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace ManagingUsers.Infrastructure
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store) : base(store)
+        {
+
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
+        {
+            AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+            return manager;
+        }
+    }
+}
