33c1133466eaa33afae24fbff64620536b68005e
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-13 22:29:52 +0100

    add admin controller

---
+++ ManagingUsers/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManagingUsers.Infrastructure;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace ManagingUsers.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(UerManager.Users);
+        }
+
+        private AppUserManager UerManager
+        {
+            get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); }
+        }
+    }
+}
