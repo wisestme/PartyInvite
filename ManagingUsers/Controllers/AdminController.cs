cd0fa15ce17c48e5b8954fd6cee8449048069cf8
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-14 00:22:06 +0100

    create user

--- ManagingUsers/Controllers/AdminController.cs
+++ ManagingUsers/Controllers/AdminController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ManagingUsers.Infrastructure;
+using ManagingUsers.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace ManagingUsers.Controllers
@@ -14,6 +16,44 @@ namespace ManagingUsers.Controllers
         public ActionResult Index()
         {
             return View(UerManager.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = UerManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
 
         private AppUserManager UerManager
