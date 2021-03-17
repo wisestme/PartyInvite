86f50e9b46b43c8248a7ee724add1d55686759de
wisestme01 <c.nwagwu@genesystechhub.com>
2021-03-17 14:46:48 +0100

    add login view model

--- ManagingUsers/Controllers/HomeController.cs
+++ ManagingUsers/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace ManagingUsers.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [Authorize]
         public ActionResult Index()
         {
             Dictionary<string, object> data = new Dictionary<string, object>();
--- ManagingUsers/Models/UserViewModels.cs
+++ ManagingUsers/Models/UserViewModels.cs
@@ -17,4 +17,13 @@ namespace ManagingUsers.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
 }
